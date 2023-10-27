801957197b9a43c3f501af9acab6f9371585a8d3
kjroelke <kj.roelke@gmail.com>
2023-10-27 14:17:49 -0500

    Hero BG & lite-youtube Fixes #8

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,8 @@
 import '../styles/main.scss';
+import 'lite-youtube-embed/src/lite-yt-embed';
+import 'lite-youtube-embed/src/lite-yt-embed.css';
 import { myCopyright } from './modules/utilities';
 
-function init() {
-	console.log(`Let's get to building!`);
+(function () {
 	myCopyright();
-}
-init();
+})();
