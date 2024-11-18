227369d804942e3484d65b87d4781dae94896fb1
kjroelke <kj.roelke@gmail.com>
2024-11-18 13:14:46 -0600

    add GA analytics with react island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
-	site: 'https://growtogether.church',
+  site: 'https://growtogether.church',
+  integrations: [react()],
 });
---
+++ src/components/ConsentBanner.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+
+export default function ConsentBanner() {
+	const userConsent = JSON.parse(localStorage.getItem('userConsent')) || {};
+	const [consent, setConsent] = useState(userConsent.analytics || false);
+
+	if (!consent) {
+		return (
+			<div className="consent-banner sticky-bottom text-bg-dark p-3">
+				<div className="container">
+					<div className="row align-items-center justify-content-between">
+						<div className="col-lg-6">
+							<p className="mb-0">
+								We're use cookies to improve user experience. By
+								clicking "I agree", you consent to our use of
+								cookies. For more information, please read our{' '}
+								<a
+									href="https://www.kingdomone.co/privacy-policy/"
+									target="_blank"
+								>
+									privacy policy
+								</a>
+								.
+							</p>
+						</div>
+						<div className="col-auto flex-shrink-1">
+							<button
+								className="btn btn-primary fs-base px-3 py-2 fw-normal"
+								onClick={() => {
+									localStorage.setItem(
+										'userConsent',
+										JSON.stringify({
+											analytics: true,
+											ads: false,
+										})
+									);
+									setConsent(true);
+									window.location.reload(); // Reload to apply consent settings
+								}}
+							>
+								I agree
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
