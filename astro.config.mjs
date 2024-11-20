f9b50bb108ac6378048e5c3909dfa952025cfbea
kjroelke <kj.roelke@gmail.com>
2024-11-19 22:39:31 -0600

    fix deploys

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
-	site: 'https://kingdom-one.github.io/giving-tuesday/',
+	site: 'https://kingdom-one.github.io',
+	base: '/giving-tuesday',
 	integrations: [react()],
 });
